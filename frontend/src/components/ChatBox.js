145d4a0837e9bb6cd0bf8ebc72998c2a6a5d23b1
Fayjul Islam <fayzulislam9@gmail.com>
2022-11-16 20:26:05 +0600

    message in now console

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 export default function ChatBox() {
   const [isOpen, setIsOpen] = useState(false);
+  const [messageBody, setMessageBody] = useState('');
   const supportHandler = (e) => {
     setIsOpen(true);
   };
   const closeHandler = (e) => {
     setIsOpen(false);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(messageBody);
   };
   return (
     <div className="chatbox">
@@ -16,13 +27,37 @@ export default function ChatBox() {
           Chat with us
         </Button>
       ) : (
-        <Button
-          className="btn-sm btn-secondary"
-          type="button"
-          onClick={closeHandler}
-        >
-          X
-        </Button>
+        <Card>
+          <Card.Body>
+            <Row>
+              <Col>
+                <strong>Support</strong>
+              </Col>
+              <Col>
+                <Button
+                  className="btn-sm btn-secondary"
+                  type="button"
+                  onClick={closeHandler}
+                >
+                  X
+                </Button>
+              </Col>
+            </Row>
+            <hr />
+            <ListGroup>
+              <ListGroup.Item>no messages</ListGroup.Item>
+            </ListGroup>
+            <form onSubmit={submitHandler}>
+              <InputGroup className="col-6"></InputGroup>
+              <FormControl
+                value={messageBody}
+                onChange={(e) => setMessageBody(e.target.value)}
+                type="text"
+                placeholder="type message"
+              ></FormControl>
+            </form>
+          </Card.Body>
+        </Card>
       )}
     </div>
   );
