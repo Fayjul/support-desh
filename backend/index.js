654792921ac0676ade16bcea445620287ec0e8b6
Fayjul Islam <fayzulislam9@gmail.com>
2022-11-21 15:39:27 +0600

    Alhamdulillah frontend is now on port 5000

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import http from 'http';
+import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 5000;
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
 });
-
+/*
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+*/
+const httpServer = http.Server(app);
+httpServer.listen(port, () => {
+  console.log(`server started at http://localhost:${port}/`);
 });
