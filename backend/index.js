53a0865ac6a9896c047d4a8e7468eee3005834ee
Fayjul Islam <fayzulislam9@gmail.com>
2022-11-24 08:29:44 +0600

    Alhamdulillah chatbox is complite with backend

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,81 @@ app.listen(port, () => {
 });
 */
 const httpServer = http.Server(app);
+
+const io = new Server(httpServer, { cors: { origin: '*' } });
+const users = [];
+
+io.on('connection', (socket) => {
+  socket.on('onLogin', (user) => {
+    const updateUser = {
+      ...user,
+      online: true,
+      socketId: socket.id,
+      message: [],
+    };
+    const existUser = users.find((x) => x.name === updateUser.name);
+    if (existUser) {
+      (existUser.socketId = socket.id), (existUser.online = true);
+    } else {
+      users.push(updateUser);
+    }
+    const admin = users.find((x) => x.name === 'Admin' && x.online);
+    if (admin) {
+      io.to(admin.socketId).emit('updateUser', updateUser);
+    }
+    if (updateUser.name === 'Admin') {
+      io.to(updateUser.socketId).emit('listUsers', users);
+    }
+  });
+  socket.on('disconnect', () => {
+    const user = users.find((x) => x.socketId === socket.id);
+    if (user) {
+      user.online = false;
+      const admin = users.find((x) => x.name === 'Admin' && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('updateUser', user);
+      }
+    }
+  });
+  socket.on('onUserSelected', (user) => {
+    const admin = users.find((x) => x.name === 'Admin' && x.online);
+    if (admin) {
+      const existUser = users.find((x) => x.name === user.name);
+      io.to(admin.socketId).emit('selectUser', existUser);
+    }
+  });
+  socket.on('onMessage', (message) => {
+    if (message.from === 'Admin') {
+      const user = users.find((x) => x.name === message.to && x.online);
+      if (user) {
+        io.to(user.socketId).emit('message', message);
+        user.message.push(message);
+      } else {
+        io.to(socket.id).emit('message', {
+          from: 'System',
+          to: 'Admin',
+          body: 'User Is not Online',
+        });
+      }
+    } else {
+      const admin = users.find((x) => x.name === 'Admin' && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('message', message);
+        const user = users.find((x) => x.name === message.from && x.online);
+        if (user) {
+          user.message.push(message);
+        }
+      } else {
+        io.to(socket.id).emit('message', {
+          from: 'System',
+          to: message.from,
+          body: 'Sorry, Admin is not online right now',
+        });
+      }
+    }
+  });
+});
+
 httpServer.listen(port, () => {
   console.log(`server started at http://localhost:${port}/`);
 });
--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import socketIOClient from 'socket.io-client';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -7,11 +8,45 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 
+const ENDPOINT =
+  window.location.host.indexOf('localhost') >= 0
+    ? 'http://127.0.0.1:5000'
+    : window.location.host;
+
 export default function ChatBox() {
+  const uiMessagesRef = useRef(null);
+
+  const [userName, setUserName] = useState();
+  const [messages, setMessages] = useState([
+    { from: 'System', body: 'Hello there, Please ask your question.' },
+  ]);
+  const [socket, setSocket] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const [messageBody, setMessageBody] = useState('');
+
+  useEffect(() => {
+    if (uiMessagesRef.current) {
+      uiMessagesRef.current.scrollBy({
+        top: uiMessagesRef.current.scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+    if (socket) {
+      socket.emit('onLogin', { name: userName });
+      socket.on('message', (data) => {
+        setMessages([...messages, data]);
+      });
+    }
+  }, [messages, socket, userName]);
+
   const supportHandler = (e) => {
     setIsOpen(true);
+    if (!userName) {
+      setUserName(prompt('Please enter your name'));
+    }
+    const sk = socketIOClient(ENDPOINT);
+    setSocket(sk);
   };
   const closeHandler = (e) => {
     setIsOpen(false);
@@ -21,7 +56,18 @@ export default function ChatBox() {
     if (!messageBody.trim()) {
       alert('Eror, Please type message');
     } else {
-      console.log(messageBody);
+      setMessages([
+        ...messages,
+        { body: messageBody, from: userName, to: 'Admin' },
+      ]);
+      setTimeout(() => {
+        socket.emit('onMessage', {
+          body: messageBody,
+          from: userName,
+          to: 'Admin',
+        });
+      }, 1000);
+      setMessageBody('');
     }
   };
   return (
@@ -48,8 +94,12 @@ export default function ChatBox() {
               </Col>
             </Row>
             <hr />
-            <ListGroup>
-              <ListGroup.Item>no messages</ListGroup.Item>
+            <ListGroup ref={uiMessagesRef}>
+              {messages.map((msg, index) => (
+                <ListGroup.Item key={index}>
+                  <strong>{`${msg.from}: `}</strong> {msg.body}
+                </ListGroup.Item>
+              ))}
             </ListGroup>
             <form onSubmit={submitHandler}>
               <InputGroup className="col-6">
@@ -66,41 +116,6 @@ export default function ChatBox() {
             </form>
           </Card.Body>
         </Card>
-        // <Card>
-        //   <Card.Body>
-        //     <Row>
-        //       <Col>
-        //         <strong>Support</strong>
-        //       </Col>
-        //       <Col className="text-end">
-        //         <Button
-        //           className="btn-sm btn-secondary"
-        //           type="button"
-        //           onClick={closeHandler}
-        //         >
-        //           x
-        //         </Button>
-        //       </Col>
-        //     </Row>
-        //     <hr />
-        //     <ListGroup>
-        //       <ListGroup.Item>no messages</ListGroup.Item>
-        //     </ListGroup>
-        //     <form onSubmit={submitHandler}>
-        //       <InputGroup className="col-6">
-        //         <FormControl
-        //           value={messageBody}
-        //           onChange={(e) => setMessageBody(e.target.value)}
-        //           type="text"
-        //           placeholder="type message"
-        //         ></FormControl>
-        //         <Button type="submit" variant="primary">
-        //           Send
-        //         </Button>
-        //       </InputGroup>
-        //     </form>
-        //   </Card.Body>
-        // </Card>
       )}
     </div>
   );
